dfe5913ec40b24686999df125f98dfafd8bde12b
Samandar Murodjonov <30470723+SamixGroup@users.noreply.github.com>
2022-04-21 13:25:26 +0500

    some optimizations

--- FirstProject/routes/web.php
+++ FirstProject/routes/web.php
@@ -24,8 +24,7 @@ Route::post('/addOrder', [\App\Http\Controllers\OrdersController::class, 'upload
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('admin', [\App\Http\Controllers\OrdersController::class, 'allOrders']
-)->name('allData')->middleware('isAdmin');
-
-Route::get('admin/orderVerify/{id}', [\App\Http\Controllers\OrdersController::class, 'orderVerify']
-)->name('orderVerify')->middleware('isAdmin');
+Route::middleware('isAdmin')->group(function() {
+   Route::get('admin', [\App\Http\Controllers\OrdersController::class, 'allOrders'])->name('allData');
+   Route::get('admin/orderVerify/{id}', [\App\Http\Controllers\OrdersController::class, 'orderVerify'])->name('orderVerify'); 
+});
